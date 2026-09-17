21cd9701aa6f596a77906a7ff7e75f264980c912
Josias <messias_jpb@hotmail.com>
2026-09-17 00:44:02 -0300

    Refactor places component: streamline card structure and enhance subplace visibility with toggle functionality

--- resources/views/livewire/communities/places.blade.php
+++ resources/views/livewire/communities/places.blade.php
@@ -1,9 +1,23 @@
 <div class="space-y-2">
     @forelse ($places as $place)
         <div wire:key="place-{{ $place->id }}">
-            @if ($place->subplaces->isNotEmpty())
-                <x-card :header="$place->name" minimize="mount" shadowless bordered>
-                    <div class="space-y-2">
+            <x-card shadowless bordered x-data="{ expanded: false }">
+                <div class="flex justify-between gap-x-4">
+                    <span>{{  $place->name }}</span>
+                    <div class="flex items-center gap-x-2">
+                        @if ($place->subplaces->isNotEmpty())
+                            <x-button @click="expanded = !expanded" icon="rectangle-group" flat />
+                        @endif
+                        <x-dropdown icon="ellipsis-horizontal-circle" flat>
+                            <x-dropdown.items text="Adicionar subespaço" />
+                            <x-dropdown.items text="Editar" />
+                            <x-dropdown.items text="Excluir" separator />
+                        </x-dropdown>
+                    </div>
+                </div>
+                @if ($place->subplaces->isNotEmpty())
+                    <div class="mt-4 space-y-2" x-show="expanded" x-collapse>
+                        <x-label label="Sub espaços" />
                         @foreach ($place->subplaces as $subplace)
                             <div wire:key="subplace-{{ $subplace->id }}">
                                 <x-card shadowless bordered>
@@ -12,12 +26,8 @@
                             </div>
                         @endforeach
                     </div>
-                </x-card>
-            @else
-                <x-card shadowless bordered>
-                    {{ $place->name }}
-                </x-card>
-            @endif
+                @endif
+            </x-card>
         </div>
     @empty
         <p class="text-sm text-gray-500 dark:text-dark-400">Nenhum espaço está cadastrado para esta comunidade.</p>
