51a55a3598d17842ab03a2695a5cbb5d283cca8b
Josias Pereira Bueno <66849846+buenojosias@users.noreply.github.com>
2026-09-18 16:33:01 -0300

    Add seeded users for each role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,26 +3,63 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
+        User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@email.com',
             'whatsapp' => null,
-            'password' => '123456',
             'is_active' => true,
             'roles' => ['admin'],
         ]);
-        User::create([
+
+        User::factory()->create([
+            'name' => 'Member',
+            'email' => 'member@email.com',
+            'whatsapp' => null,
+            'is_active' => true,
+            'roles' => ['member'],
+        ]);
+
+        User::factory()->create([
+            'name' => 'Secretary',
+            'email' => 'secretary@email.com',
+            'whatsapp' => null,
+            'is_active' => true,
+            'roles' => ['secretary'],
+        ]);
+
+        User::factory()->create([
+            'name' => 'Pascom',
+            'email' => 'pascom@email.com',
+            'whatsapp' => null,
+            'is_active' => true,
+            'roles' => ['pascom'],
+        ]);
+
+        User::factory()->create([
+            'name' => 'CPP',
+            'email' => 'cpp@email.com',
+            'whatsapp' => null,
+            'is_active' => true,
+            'roles' => ['cpp'],
+        ]);
+
+        User::factory()->create([
+            'name' => 'Priest',
+            'email' => 'priest@email.com',
+            'whatsapp' => null,
+            'is_active' => true,
+            'roles' => ['priest'],
+        ]);
+
+        User::factory()->create([
             'name' => 'Josias Bueno',
             'email' => 'josias@email.com',
-            'whatsapp' => '41996881818',
-            'password' => '123456',
             'is_active' => true,
             'roles' => ['member'],
         ]);
